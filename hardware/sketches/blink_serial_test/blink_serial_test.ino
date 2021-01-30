0e356793080345e7bbf53085873f013f5751e21e
Malcolm Lashley <mlashley@gmail.com>
2021-01-30 08:54:33 +0000

    Add CV-step test, set fPWM=32kHz

--- hardware/sketches/blink_serial_test/blink_serial_test.ino
+++ hardware/sketches/blink_serial_test/blink_serial_test.ino
@@ -1,6 +1,9 @@
 // Uncomment to test MIDI, else test serial-print/echo.
 // #define TEST_MIDI 1
 
+// Uncomment to test CV steps in serial-print mode, otherwise simple on/off.
+#define TEST_STEP_CV
+
 #ifdef TEST_MIDI
   #include <MIDI.h>
   MIDI_CREATE_DEFAULT_INSTANCE();
@@ -70,7 +73,8 @@ void setup() {
   MIDI.setHandleNoteOff(handleNoteOff);
   MIDI.begin(MIDI_CHANNEL_OMNI);
 #endif
-
+  TCCR1B = TCCR1B & B11111000 | B00000001; // set timer 1 divisor to 1 for PWM frequency of 31372.55 Hz
+  TCCR2B = TCCR2B & B11111000 | B00000001;
   Serial.begin(115200);
   Serial.println("Starting self-test");
 }
@@ -88,6 +92,7 @@ void loop() {
     delay(100);
     digitalWrite(ledPin,LOW);
 
+#ifndef TEST_STEP_CV
     /* also cycle PWM pins on/off every second */
     if(pwmstate == LOW) {
       pwmstate = HIGH;
@@ -100,6 +105,18 @@ void loop() {
     digitalWrite(CV4,pwmstate);
   }
 #else
+    // pwmstate += 0x40; // this will wrap for 0, 25%, 50% 75% duty cycle
+    pwmstate += 0x20; // this will wrap for 0, 12.5, 25% ... 87.5% duty cycle
+    analogWrite(CV1,pwmstate);
+    analogWrite(CV2,pwmstate);
+    analogWrite(CV3,pwmstate);
+    analogWrite(CV4,pwmstate);
+    next += 1000; // hacked in.. to let the multimeter settle - will also impact blink-freq.
+  }
+
+#endif // TEST_STEP_CV
+  
+#else
   MIDI.read();
-#endif
+#endif // TEST_MIDI
 }
