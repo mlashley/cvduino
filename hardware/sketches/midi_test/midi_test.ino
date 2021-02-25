fc5ca7dccfc07dc7f394f42cf99a1a0907376c71
Malcolm Lashley <mlashley@gmail.com>
2021-02-25 23:58:06 +0000

    Switch to FastPWM

--- hardware/sketches/midi_test/midi_test.ino
+++ hardware/sketches/midi_test/midi_test.ino
@@ -29,10 +29,16 @@ void handleNoteOn(byte channel, byte note, byte velocity)
   digitalWrite(ledPin,HIGH);
   int duty = noteToPWM(note);
   digitalWrite(CV1,HIGH); // GATE
+
+  // NB FastPWM doesn't have a zero duty-cycle...
   if(duty == 0) {
     digitalWrite(CV2,LOW);
     digitalWrite(CV3,LOW);
   } else {
+    // Need to re-write control-registers again potentially clobbered by digitalWrite above.
+    TCCR1A = B10100011; // COM1A[1:0] = B10, COM1B[1:0] = B10, xx, WGM11:10 = B11
+    TCCR1B = TCCR1B & B11100000 | B00001001; // WGM[13:12] = B01, set timer 1 divisor (CS12:10) to 001 for PWM frequency of 15625.00 Hz (16MHz / 1024 with 10 bit mode fast PWM)
+
     OCR1A = duty; // CV2
     OCR1B = duty-(12*STEPSPERSEMI) ; // CV3 = -Octv.
   }
@@ -54,14 +60,14 @@ void setup() {
   pinMode(CV3, OUTPUT);
   pinMode(CV4, OUTPUT);
 
-  // Timer1 WGM[13:10] = 0011 PWM, phase correct, 10-bit.
+  // Timer1 WGM[13:10] = 0111 Fast PWM, , 10-bit.
   // COM1A/B[10] = 10 Clear OC1A/OC1B on compare match when up-counting. Set OC1A/OC1B on compare match when down counting.
   // CS[12:10] = B001 , clkIO/1 (no prescaling)
 
   // TCCR1A = COM1A1,A0,COM1B1,B0,x,x,WGM11,10
   // TCCR1B = ICNC1, ICES2,x,WGM13,12,CS12,11,10
   TCCR1A = B10100011; // COM1A[1:0] = B10, COM1B[1:0] = B10, xx, WGM11:10 = B11
-  TCCR1B = TCCR1B & B11111000 | B00000001; // set timer 1 divisor (CS12:10) to 001 for PWM frequency of 7820.13 Hz (16MHz / 2046 with 10 bit mode phase-correct PWM)
+  TCCR1B = TCCR1B & B11100000 | B00001001; // WGM[13:12] = B01, set timer 1 divisor (CS12:10) to 001 for PWM frequency of 15625.00 Hz (16MHz / 1024 with 10 bit mode fast PWM)
   TCCR2B = TCCR2B & B11111000 | B00000001; // set timer 2 divisor (CS12:10) to 001 for PWM frequency of 31372.55 Hz (16MHz / 510 with 8 bit mode phase-correct PWM)
 
   // Selftest
